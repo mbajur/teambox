d92f1e02f2b401114272062fa3846b386fe6e78d
mbajur <mbajur@gmail.com>
2025-07-02 23:31:25 +0200

    Comment out Preview class in Emailer for future reference

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -261,135 +261,135 @@ class Emailer < ActionMailer::Base
   end
 
   # requires data from rake db:seed
-  class Preview < MailView
-    def notify_task
-      task = Task.find_by_name "Contact area businesses for banner exchange"
-      ::Emailer.notify_task(task.user.id, task.project.id, task.id)
-    end
+  # class Preview < MailView
+  #   def notify_task
+  #     task = Task.find_by_name "Contact area businesses for banner exchange"
+  #     ::Emailer.notify_task(task.user.id, task.project.id, task.id)
+  #   end
 
-    def notify_conversation
-      conversation = Conversation.find_by_name "Seth Godin's 'What matters now'"
-      ::Emailer.notify_conversation(conversation.user.id, conversation.project.id, conversation.id)
-    end
+  #   def notify_conversation
+  #     conversation = Conversation.find_by_name "Seth Godin's 'What matters now'"
+  #     ::Emailer.notify_conversation(conversation.user.id, conversation.project.id, conversation.id)
+  #   end
 
-    def notify_activity_on_page
-      activity = Activity.where(target_type: "Page").first
-      ::Emailer.notify_activity(activity.user_id, activity.project_id, activity.id)
-    end
+  #   def notify_activity_on_page
+  #     activity = Activity.where(target_type: "Page").first
+  #     ::Emailer.notify_activity(activity.user_id, activity.project_id, activity.id)
+  #   end
 
-    def notify_activity_on_note
-      activity = Activity.where(target_type: "Note").first
-      ::Emailer.notify_activity(activity.user_id, activity.project_id, activity.id)
-    end
+  #   def notify_activity_on_note
+  #     activity = Activity.where(target_type: "Note").first
+  #     ::Emailer.notify_activity(activity.user_id, activity.project_id, activity.id)
+  #   end
 
-    def project_digest
-      project_id  = Project.first
-      person_id   = Project.first.people.first.id
-      user        = Project.first.users.first
-      user_id     = user.id
+  #   def project_digest
+  #     project_id  = Project.first
+  #     person_id   = Project.first.people.first.id
+  #     user        = Project.first.users.first
+  #     user_id     = user.id
 
-      target_types_and_ids = []
-      comment_ids = []
+  #     target_types_and_ids = []
+  #     comment_ids = []
 
-      user.notifications.each do |notification|
-        target_types_and_ids << { target_type: notification.target_type, target_id: notification.target_id }
-        comment_ids << notification.comment_id unless notification.comment_id.nil?
-      end
+  #     user.notifications.each do |notification|
+  #       target_types_and_ids << { target_type: notification.target_type, target_id: notification.target_id }
+  #       comment_ids << notification.comment_id unless notification.comment_id.nil?
+  #     end
 
-      target_types_and_ids.uniq!
-      comment_ids = comment_ids[(comment_ids.size/3) .. (comment_ids.size)].uniq
+  #     target_types_and_ids.uniq!
+  #     comment_ids = comment_ids[(comment_ids.size/3) .. (comment_ids.size)].uniq
 
-      ::Emailer.project_digest(user_id, person_id, project_id, target_types_and_ids, comment_ids)
-    end
+  #     ::Emailer.project_digest(user_id, person_id, project_id, target_types_and_ids, comment_ids)
+  #   end
 
-    def daily_task_reminder
-      user = User.find_by_login "frank"
-      ::Emailer.daily_task_reminder(user.id)
-    end
+  #   def daily_task_reminder
+  #     user = User.find_by_login "frank"
+  #     ::Emailer.daily_task_reminder(user.id)
+  #   end
 
-    def signup_invitation
-      other_invitation = Invitation.new do |i|
-        i.invited_user = User.create!(login: "pepito", password: "papapa", password_confirmation: "papapa", first_name: "Pepito", last_name: "Delospalotes", email: "pepito@teambox.com") rescue User.find_by_login("pepito")
-        i.token = ActiveSupport::SecureRandom.hex(20)
-        i.user = User.first
-        i.project = Project.first
-      end
-      other_invitation.save!(false)
-      invitation = Invitation.new do |i|
-        i.email = "test@teambox.com"
-        i.token = ActiveSupport::SecureRandom.hex(20)
-        i.user = User.first
-        i.project = Project.first
-      end
-      invitation.save!(false)
-      ::Emailer.signup_invitation(invitation.id)
-    end
+  #   def signup_invitation
+  #     other_invitation = Invitation.new do |i|
+  #       i.invited_user = User.create!(login: "pepito", password: "papapa", password_confirmation: "papapa", first_name: "Pepito", last_name: "Delospalotes", email: "pepito@teambox.com") rescue User.find_by_login("pepito")
+  #       i.token = ActiveSupport::SecureRandom.hex(20)
+  #       i.user = User.first
+  #       i.project = Project.first
+  #     end
+  #     other_invitation.save!(false)
+  #     invitation = Invitation.new do |i|
+  #       i.email = "test@teambox.com"
+  #       i.token = ActiveSupport::SecureRandom.hex(20)
+  #       i.user = User.first
+  #       i.project = Project.first
+  #     end
+  #     invitation.save!(false)
+  #     ::Emailer.signup_invitation(invitation.id)
+  #   end
 
-    def reset_password
-      user = User.first
-      ::Emailer.reset_password(user.id)
-    end
+  #   def reset_password
+  #     user = User.first
+  #     ::Emailer.reset_password(user.id)
+  #   end
 
-    def forgot_password
-      password_reset = ResetPassword.create! do |passwd|
-        passwd.email = "reset#{ActiveSupport::SecureRandom.hex(20)}@example.com"
-        passwd.user = User.first
-        passwd.reset_code = ActiveSupport::SecureRandom.hex(20)
-      end
-      ::Emailer.forgot_password(password_reset.id)
-    end
+  #   def forgot_password
+  #     password_reset = ResetPassword.create! do |passwd|
+  #       passwd.email = "reset#{ActiveSupport::SecureRandom.hex(20)}@example.com"
+  #       passwd.user = User.first
+  #       passwd.reset_code = ActiveSupport::SecureRandom.hex(20)
+  #     end
+  #     ::Emailer.forgot_password(password_reset.id)
+  #   end
 
-    def project_membership_notification
-      invitation = Invitation.new do |i|
-        i.user = User.first
-        i.invited_user = User.last
-        i.project = Project.first
-      end
-      invitation.save!(false)
-      ::Emailer.project_membership_notification(invitation.id)
-    end
+  #   def project_membership_notification
+  #     invitation = Invitation.new do |i|
+  #       i.user = User.first
+  #       i.invited_user = User.last
+  #       i.project = Project.first
+  #     end
+  #     invitation.save!(false)
+  #     ::Emailer.project_membership_notification(invitation.id)
+  #   end
 
-    def project_invitation
-      invitation = Invitation.new do |i|
-        i.token = ActiveSupport::SecureRandom.hex(20)
-        i.user = User.first
-        i.invited_user = User.last
-        i.project = Project.first
-      end
-      invitation.is_silent = true
-      invitation.save!(false)
-      ::Emailer.project_invitation(invitation.id)
-    end
+  #   def project_invitation
+  #     invitation = Invitation.new do |i|
+  #       i.token = ActiveSupport::SecureRandom.hex(20)
+  #       i.user = User.first
+  #       i.invited_user = User.last
+  #       i.project = Project.first
+  #     end
+  #     invitation.is_silent = true
+  #     invitation.save!(false)
+  #     ::Emailer.project_invitation(invitation.id)
+  #   end
 
-    def accepted_project_invitation
-      invitation = Invitation.new do |i|
-        i.token = ActiveSupport::SecureRandom.hex(20)
-        i.user = User.first
-        i.invited_user = User.last
-        i.project = Project.first
-      end
-      invitation.is_silent = true
-      invitation.save!(false)
-      ::Emailer.accepted_project_invitation(invitation.id)
-    end
+  #   def accepted_project_invitation
+  #     invitation = Invitation.new do |i|
+  #       i.token = ActiveSupport::SecureRandom.hex(20)
+  #       i.user = User.first
+  #       i.invited_user = User.last
+  #       i.project = Project.first
+  #     end
+  #     invitation.is_silent = true
+  #     invitation.save!(false)
+  #     ::Emailer.accepted_project_invitation(invitation.id)
+  #   end
 
-    def confirm_email
-      user = User.first
-      ::Emailer.confirm_email(user.id)
-    end
+  #   def confirm_email
+  #     user = User.first
+  #     ::Emailer.confirm_email(user.id)
+  #   end
 
 
-    def public_download
-      upload = Upload.new do |u|
-        u.asset_file_name = "Somefile.txt"
-        u.user = User.first
-        u.project = Project.first
-      end
-      upload.save!
+  #   def public_download
+  #     upload = Upload.new do |u|
+  #       u.asset_file_name = "Somefile.txt"
+  #       u.user = User.first
+  #       u.project = Project.first
+  #     end
+  #     upload.save!
 
-      ::Emailer.public_download(upload.id, "someone@teambox.com", "upload")
-    end
-  end
+  #     ::Emailer.public_download(upload.id, "someone@teambox.com", "upload")
+  #   end
+  # end
 
   private
 
