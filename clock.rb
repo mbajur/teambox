8f0a71734c9fe2781e4814df1dd4a127beccda2b
mbajur <mbajur@gmail.com>
2025-08-25 08:57:37 +0200

    Add scheduled time for daily task reminders and digest emails

--- clock.rb
+++ clock.rb
@@ -4,11 +4,11 @@ require './config/environment'
 require 'active_support/time' # Allow numeric durations (eg: 1.minutes)
 
 module Clockwork
-  every(1.hour, 'user.daily_task_reminders') do
+  every(1.hour, 'user.daily_task_reminders', at: "**:00") do
     User.send_daily_task_reminders
   end
 
-  every(1.hour, 'person.send_all_digest') do
+  every(1.hour, 'person.send_all_digest', at: "**:00") do
     Person.send_all_digest
   end
 end
