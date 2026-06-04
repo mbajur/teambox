682ef0909f970efa110bc44d5a0abb237b2e44ab
mbajur <mbajur@gmail.com>
2026-06-04 09:36:43 +0200

    Add Cucumber job to CI workflow and adjust Capybara wait time for CI environment

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -54,17 +54,28 @@ require 'capybara-screenshot/cucumber'
 Capybara::Screenshot.prune_strategy = :keep_last_run
 
 Capybara.javascript_driver = :cuprite
-Capybara.default_max_wait_time = 5
+Capybara.default_max_wait_time = ENV["CI"] ? 10 : 5
 Capybara.register_driver(:cuprite) do |app|
   opts = {
     window_size: [ 1200, 800 ],
     timeout: 30
   }
 
+  if ENV["CI"]
+    opts.merge!(
+      browser_options: {
+        "no-sandbox": nil,
+        "disable-dev-shm-usage": nil,
+        "disable-gpu": nil
+      },
+      process_timeout: 60
+    )
+  end
+
   opts.merge!(
     headless: false,
     slowmo: 0.3
-  ) if ENV['DEBUG']
+  ) if ENV["DEBUG"]
 
   Capybara::Cuprite::Driver.new(app, opts)
 end
