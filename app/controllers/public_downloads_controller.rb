8cafb7428208622836b45146c7e105570d3dbabc
mbajur <mbajur@gmail.com>
2026-06-05 09:25:08 +0200

    Remove confirmed_user? from skip_before_action in PublicDownloadsController

--- app/controllers/public_downloads_controller.rb
+++ app/controllers/public_downloads_controller.rb
@@ -2,7 +2,6 @@ class PublicDownloadsController < ApplicationController
   include Downloads::Downloading
 
   skip_before_action :set_locale,
-                     :confirmed_user?,
                      :load_project,
                      :load_organizations,
                      :set_client,
